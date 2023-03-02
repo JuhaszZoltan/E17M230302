57463db5d607acd6156f206d60557751d3ce8c5e
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-03-02 10:57:00 +0100

    Add project files.

---
+++ IdozitettFelirat.cs
@@ -0,0 +1,38 @@
+﻿namespace E17M230302
+{
+    internal class IdozitettFelirat
+    {
+        public string Idozites { get; set; }
+        public string Felirat { get; set; }
+        public string SrtIdozites
+        {
+            get 
+            {
+                var ti = Idozites.Split(" - ");
+
+                var top = ti[0].Split(':');
+                TimeSpan tol = new(
+                    hours: 0,
+                    minutes: int.Parse(top[0]),
+                    seconds: int.Parse(top[1]));
+
+                var iop = ti[1].Split(':');
+                TimeSpan ig = new(
+                    hours: 0,
+                    minutes: int.Parse(iop[0]),
+                    seconds: int.Parse(iop[1]));
+
+                return $"{tol} --> {ig}";
+            }
+        }
+
+
+        public int SzavakSzama => Felirat.Split().Length;
+
+        public IdozitettFelirat(string idozites, string felirat)
+        {
+            Idozites = idozites;
+            Felirat = felirat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using E17M230302;
+using System.Text;
+
+List<IdozitettFelirat> feliratok = new();
+
+using StreamReader sr = new("..\\..\\..\\RES\\feliratok.txt", Encoding.UTF8);
+while (!sr.EndOfStream)
+{
+    feliratok.Add(new IdozitettFelirat(
+        idozites: sr.ReadLine(),
+        felirat: sr.ReadLine()));
+}
+
+Console.WriteLine($"5. feladat: felriatok száma: {feliratok.Count}");
+
+var feladat7 = feliratok.MaxBy(f => f.SzavakSzama);
+Console.WriteLine("7. feladat: legtöbb szóból álló felirat:");
+Console.WriteLine(feladat7.Felirat);
+
+
+using StreamWriter sw = new("..\\..\\..\\RES\\felirat.srt", append: false, Encoding.UTF8);
+int ssz = 1;
+foreach (var f in feliratok)
+{
+    sw.WriteLine(ssz);
+    sw.WriteLine(f.SrtIdozites);
+    sw.WriteLine(f.Felirat);
+    sw.Write('\n');
+    ssz++;
+}
+
+
